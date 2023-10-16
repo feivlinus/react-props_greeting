e9a469463b09a3fc1c13913f9694bfecd4cfd4d6
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-16 16:43:03 +0200

    Challenge completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Felix" />;
 }
+
+function Greeting(probs) {
+  let nameToGreet = probs.name;
+  if (nameToGreet === "CJ") {
+    nameToGreet = "Coach";
+  }
+
+  return (
+    <h1>
+      Hello, <span>{nameToGreet}</span>!
+    </h1>
+  );
+}
+
+//Alternative
+// function Greeting({ name }) {
+//   return (
+//     <h1>
+//       Hello, <span>{name}</span>!
+//     </h1>
+//   );
+// }
